716ac101b87b25ca02a544bc935d5561963a12de
Logan Ward <ward.logan.t@gmail.com>
2018-08-31 10:42:33 -0500

    Upgraded the schema checker to dereference paths

--- validate_schemas.py
+++ validate_schemas.py
@@ -1,14 +1,20 @@
-from jsonschema import Draft4Validator
+from jsonschema import Draft4Validator, RefResolver
+import jsonref
 import glob
-import json
+import os
 
 # Find all the schemas
-schemas = glob.glob('schemas/**/*.json', recursive=True)
+schema_path = os.path.abspath('schemas')
+schemas = glob.glob('{}/**/*.json'.format(schema_path), recursive=True)
 
 # Loop through to make sure they are all valid
 for schema in schemas:
-    print('Checking {}...'.format(schema), end="")
+    print('Checking {}...'.format(os.path.relpath(schema, '.')), end="")
+
+    # Load in the schema
     with open(schema) as fp:
-        schema = json.load(fp)
-        Draft4Validator.check_schema(schema)
+        schema = jsonref.load(fp, base_uri='file:///{}/'.format(schema_path))
+
+    # Pull in the references
+    Draft4Validator.check_schema(schema)
     print('OK')
